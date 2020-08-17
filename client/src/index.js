04da8a9f13e18d4955eeb8cf5f08af0e372cc3a7
shelly ambar <ambarshelly@gmail.com>
2020-08-17 17:52:23 +0300

    in client -> adding reducers files, adding redux dev tools, adding store for redux.

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,28 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import promiseMiddleWare from "redux-promise";
+import reduxThunk from "redux-thunk";
+import Reducers from "./storage/ReduxStore/Reducers/index";
 
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleWare,
+  reduxThunk
+)(createStore);
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider
+    store={createStoreWithMiddleware(
+      Reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
